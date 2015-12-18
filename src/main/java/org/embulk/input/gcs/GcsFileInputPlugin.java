9bee1befab242b0ca28084a9b646910990cdeb21
sakama <satoshiakama@gmail.com>
2015-12-18 15:13:21 +0900

    Added retry logic to file open method

--- src/main/java/org/embulk/input/gcs/GcsFileInputPlugin.java
+++ src/main/java/org/embulk/input/gcs/GcsFileInputPlugin.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.io.InputStream;
 import java.math.BigInteger;
 
@@ -14,6 +15,7 @@ import com.google.common.base.Function;
 import com.google.common.base.Throwables;
 import com.google.common.base.Charsets;
 import com.google.common.io.BaseEncoding;
+import com.google.common.annotations.VisibleForTesting;
 import java.security.GeneralSecurityException;
 
 import org.embulk.config.TaskReport;
@@ -31,6 +33,10 @@ import org.embulk.spi.FileInputPlugin;
 import org.embulk.spi.TransactionalFileInput;
 import org.embulk.spi.unit.LocalFile;
 import org.embulk.spi.util.InputStreamFileInput;
+import org.embulk.spi.util.ResumableInputStream;
+import org.embulk.spi.util.RetryExecutor.Retryable;
+import org.embulk.spi.util.RetryExecutor.RetryGiveupException;
+import static org.embulk.spi.util.RetryExecutor.retryExecutor;
 
 import org.slf4j.Logger;
 
@@ -278,6 +284,73 @@ public class GcsFileInputPlugin
         return new GcsFileInput(task, taskIndex);
     }
 
+    @VisibleForTesting
+    static class GcsInputStreamReopener
+            implements ResumableInputStream.Reopener
+    {
+        private final Logger log = Exec.getLogger(GcsInputStreamReopener.class);
+        private final Storage client;
+        private final String bucket;
+        private final String key;
+
+        public GcsInputStreamReopener(Storage client, String bucket, String key)
+        {
+            this.client = client;
+            this.bucket = bucket;
+            this.key = key;
+        }
+
+        @Override
+        public InputStream reopen(final long offset, final Exception closedCause) throws IOException
+        {
+            try {
+                return retryExecutor()
+                    .withRetryLimit(3)
+                    .withInitialRetryWait(500)
+                    .withMaxRetryWait(30 * 1000)
+                    .runInterruptible(new Retryable<InputStream>() {
+                        @Override
+                        public InputStream call() throws InterruptedIOException, IOException
+                        {
+                            log.warn(String.format("GCS read failed. Retrying GET request with %,d bytes offset", offset), closedCause);
+                            Storage.Objects.Get getObject = client.objects().get(bucket, key);
+                            return getObject.executeMediaAsInputStream();
+                        }
+
+                        @Override
+                        public boolean isRetryableException(Exception exception)
+                        {
+                            return true;  // TODO
+                        }
+
+                        @Override
+                        public void onRetry(Exception exception, int retryCount, int retryLimit, int retryWait)
+                                throws RetryGiveupException
+                        {
+                            String message = String.format("GCS GET request failed. Retrying %d/%d after %d seconds. Message: %s",
+                                    retryCount, retryLimit, retryWait/1000, exception.getMessage());
+                            if (retryCount % 3 == 0) {
+                                log.warn(message, exception);
+                            } else {
+                                log.warn(message);
+                            }
+                        }
+
+                        @Override
+                        public void onGiveup(Exception firstException, Exception lastException)
+                                throws RetryGiveupException
+                        {
+                        }
+                    });
+            } catch (RetryGiveupException ex) {
+                Throwables.propagateIfInstanceOf(ex.getCause(), IOException.class);
+                throw Throwables.propagate(ex.getCause());
+            } catch (InterruptedException ex) {
+                throw new InterruptedIOException();
+            }
+        }
+    }
+
     public class GcsFileInput
             extends InputStreamFileInput
             implements TransactionalFileInput
@@ -322,7 +395,7 @@ public class GcsFileInputPlugin
             opened = true;
             Storage.Objects.Get getObject = client.objects().get(bucket, key);
 
-            return getObject.executeMediaAsInputStream();
+            return new ResumableInputStream(getObject.executeMediaAsInputStream(), new GcsInputStreamReopener(client, bucket, key));
         }
 
         @Override
