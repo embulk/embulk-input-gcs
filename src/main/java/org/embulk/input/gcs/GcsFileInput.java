d2b9580eba32c7755d6aae71a1d963626ffc6a7c
Robert Nguyen <ng.hung83@gmail.com>
2019-01-02 11:34:46 +0700

    Rebase `master`

--- src/main/java/org/embulk/input/gcs/GcsFileInput.java
+++ src/main/java/org/embulk/input/gcs/GcsFileInput.java
@@ -1,34 +1,21 @@
 package org.embulk.input.gcs;
 
-import com.google.api.client.http.HttpResponseException;
-import com.google.api.services.storage.Storage;
-import com.google.api.services.storage.model.Bucket;
-import com.google.api.services.storage.model.Objects;
-import com.google.api.services.storage.model.StorageObject;
-import com.google.common.base.Charsets;
-import com.google.common.io.BaseEncoding;
-import org.embulk.config.ConfigException;
+import com.google.api.gax.paging.Page;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
 import org.embulk.config.TaskReport;
 import org.embulk.spi.Exec;
 import org.embulk.spi.TransactionalFileInput;
-import org.embulk.spi.unit.LocalFile;
 import org.embulk.spi.util.InputStreamFileInput;
 import org.slf4j.Logger;
-
-import java.io.IOException;
-import java.math.BigInteger;
-import java.security.GeneralSecurityException;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 public class GcsFileInput
         extends InputStreamFileInput
         implements TransactionalFileInput
 {
-    private static final Logger log = Exec.getLogger(org.embulk.input.gcs.GcsFileInput.class);
+    private static final Logger LOG = Exec.getLogger(org.embulk.input.gcs.GcsFileInput.class);
 
-    public GcsFileInput(PluginTask task, int taskIndex)
+    GcsFileInput(PluginTask task, int taskIndex)
     {
         super(task.getBufferAllocator(), new SingleFileProvider(task, taskIndex));
     }
@@ -47,149 +34,51 @@ public class GcsFileInput
     {
     }
 
-    public static GcsAuthentication newGcsAuth(PluginTask task)
-    {
-        try {
-            return new GcsAuthentication(
-                    task.getAuthMethod().getString(),
-                    task.getServiceAccountEmail(),
-                    task.getP12Keyfile().map(localFileToPathString()),
-                    task.getJsonKeyfile().map(localFileToPathString()),
-                    task.getApplicationName()
-            );
-        }
-        catch (GeneralSecurityException | IOException ex) {
-            throw new ConfigException(ex);
-        }
-    }
-
-    protected static Storage newGcsClient(final PluginTask task, final GcsAuthentication auth)
-    {
-        Storage client = null;
-        try {
-            client = auth.getGcsClient(task.getBucket(), task.getMaxConnectionRetry());
-        }
-        catch (IOException ex) {
-            throw new ConfigException(ex);
-        }
-
-        return client;
-    }
-
-    private static Function<LocalFile, String> localFileToPathString()
-    {
-        return new Function<LocalFile, String>()
-        {
-            public String apply(LocalFile file)
-            {
-                return file.getPath().toString();
-            }
-        };
-    }
-
-    public static FileList listFiles(PluginTask task, Storage client)
+    static FileList listFiles(PluginTask task, Storage client)
     {
         String bucket = task.getBucket();
 
         FileList.Builder builder = new FileList.Builder(task);
-        listGcsFilesByPrefix(builder, client, bucket, task.getPathPrefix().get(), task.getLastPath());
+        listGcsFilesByPrefix(builder, client, bucket, task.getPathPrefix().orElse(null), task.getLastPath().orElse(null));
         return builder.build();
     }
 
     /**
      * Lists GCS filenames filtered by prefix.
-     *
+     * <p>
      * The resulting list does not include the file that's size == 0.
      */
-    public static void listGcsFilesByPrefix(FileList.Builder builder, Storage client, String bucket,
-                                             String prefix, Optional<String> lastPath)
+    static void listGcsFilesByPrefix(FileList.Builder builder, Storage client, String bucket,
+                                     String prefix, String lastPath)
     {
-        String lastKey = lastPath.isPresent() ? base64Encode(lastPath.get()) : null;
-
-        // @see https://cloud.google.com/storage/docs/json_api/v1/objects#resource
-        if (log.isDebugEnabled()) {
-            try {
-                Storage.Buckets.Get getBucket = client.buckets().get(bucket);
-                getBucket.setProjection("full");
-                Bucket bk = getBucket.execute();
-
-                log.debug("bucket name: " + bucket);
-                log.debug("bucket location: " + bk.getLocation());
-                log.debug("bucket timeCreated: " + bk.getTimeCreated());
-                log.debug("bucket owner: " + bk.getOwner());
-            }
-            catch (IOException e) {
-                log.warn("Could not access to bucket:" + bucket);
-                log.warn(e.getMessage());
-            }
+        // @see https://cloud.google.com/storage/docs/json_api/v1/buckets/get
+        if (LOG.isDebugEnabled()) {
+            printBucketInfo(client, bucket);
         }
 
-        try {
-            // @see https://cloud.google.com/storage/docs/json_api/v1/objects/list
-            Storage.Objects.List listObjects = client.objects().list(bucket);
-            listObjects.setPrefix(prefix);
-            listObjects.setPageToken(lastKey);
-            do {
-                Objects objects = listObjects.execute();
-                List<StorageObject> items = objects.getItems();
-                if (items == null) {
-                    log.info(String.format("No file was found in bucket:%s prefix:%s", bucket, prefix));
-                    break;
-                }
-                for (StorageObject o : items) {
-                    if (o.getSize().compareTo(BigInteger.ZERO) > 0) {
-                        builder.add(o.getName(), o.getSize().longValue());
-                    }
-                    log.debug("filename: " + o.getName());
-                    log.debug("updated: " + o.getUpdated());
-                }
-                lastKey = objects.getNextPageToken();
-                listObjects.setPageToken(lastKey);
-            } while (lastKey != null);
-        }
-        catch (IOException e) {
-            if ((e instanceof HttpResponseException) && ((HttpResponseException) e).getStatusCode() == 400) {
-                throw new ConfigException(String.format("Files listing failed: bucket:%s, prefix:%s, last_path:%s", bucket, prefix, lastKey), e);
+        // @see https://cloud.google.com/storage/docs/json_api/v1/objects/list
+        Page<Blob> blobs = client.list(bucket, Storage.BlobListOption.prefix(prefix), Storage.BlobListOption.pageToken(lastPath));
+        for (Blob blob : blobs.iterateAll()) {
+            if (blob.getSize() > 0) {
+                builder.add(blob.getName(), blob.getSize());
             }
-
-            log.warn(String.format("Could not get file list from bucket:%s", bucket));
-            log.warn(e.getMessage());
+            LOG.debug("filename: " + blob.getName());
+            LOG.debug("updated: " + blob.getUpdateTime());
         }
     }
 
-    // String nextToken = base64Encode(0x0a + 0x01~0x27 + filePath);
-    private static String base64Encode(String path)
+    private static void printBucketInfo(Storage client, String bucket)
     {
-        byte[] encoding;
-        byte[] utf8 = path.getBytes(Charsets.UTF_8);
-        log.debug(String.format("path string: %s ,path length:%s \" + ", path, utf8.length));
-
-        encoding = new byte[utf8.length + 2];
-        encoding[0] = 0x0a;
-        encoding[1] = new Byte(String.valueOf(path.length()));
-        System.arraycopy(utf8, 0, encoding, 2, utf8.length);
-
-        String s = BaseEncoding.base64().encode(encoding);
-        log.debug(String.format("last_path(base64 encoded): %s", s));
-        return s;
-    }
-
-    public enum AuthMethod
-    {
-        private_key("private_key"),
-        compute_engine("compute_engine"),
-        json_key("json_key");
-
-        private final String string;
-
-        AuthMethod(String string)
-        {
-            this.string = string;
-        }
-
-        public String getString()
-        {
-            return string;
-        }
+        // get Bucket
+        Storage.BucketGetOption fields = Storage.BucketGetOption.fields(
+                Storage.BucketField.LOCATION,
+                Storage.BucketField.TIME_CREATED,
+                Storage.BucketField.OWNER
+        );
+        com.google.cloud.storage.Bucket bk = client.get(bucket, fields);
+        LOG.debug("bucket name: " + bk.getName());
+        LOG.debug("bucket location: " + bk.getLocation());
+        LOG.debug("bucket timeCreated: " + bk.getCreateTime());
+        LOG.debug("bucket owner: " + bk.getOwner());
     }
 }
--- src/main/java/org/embulk/input/gcs/SingleFileProvider.java
+++ src/main/java/org/embulk/input/gcs/SingleFileProvider.java
@@ -1,23 +1,15 @@
 package org.embulk.input.gcs;
 
-import com.google.api.client.util.IOUtils;
-import com.google.api.services.storage.Storage;
-import com.google.common.base.Throwables;
-import org.embulk.spi.Exec;
+import com.google.cloud.ReadChannel;
+import com.google.cloud.storage.Storage;
 import org.embulk.spi.util.InputStreamFileInput;
 import org.embulk.spi.util.InputStreamFileInput.InputStreamWithHints;
-import org.embulk.spi.util.RetryExecutor;
-import org.slf4j.Logger;
+import org.embulk.spi.util.ResumableInputStream;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.Channels;
 import java.util.Iterator;
-
-import static org.embulk.spi.util.RetryExecutor.retryExecutor;
 
 public class SingleFileProvider
         implements InputStreamFileInput.Provider
@@ -25,20 +17,17 @@ public class SingleFileProvider
     private final Storage client;
     private final String bucket;
     private final Iterator<String> iterator;
-    private final int maxConnectionRetry;
     private boolean opened = false;
-    private final Logger log = Exec.getLogger(SingleFileProvider.class);
 
-    public SingleFileProvider(PluginTask task, int taskIndex)
+    SingleFileProvider(PluginTask task, int taskIndex)
     {
-        this.client = GcsFileInput.newGcsClient(task, GcsFileInput.newGcsAuth(task));
+        this.client = ServiceUtils.newClient(task.getJsonKeyfile());
         this.bucket = task.getBucket();
         this.iterator = task.getFiles().get(taskIndex).iterator();
-        this.maxConnectionRetry = task.getMaxConnectionRetry();
     }
 
     @Override
-    public InputStreamWithHints openNextWithHints() throws IOException
+    public InputStreamWithHints openNextWithHints()
     {
         if (opened) {
             return null;
@@ -48,10 +37,9 @@ public class SingleFileProvider
             return null;
         }
         String key = iterator.next();
-        File tempFile = Exec.getTempFileSpace().createTempFile();
-        getRemoteContentsWithRetry(tempFile, client, bucket, key, maxConnectionRetry);
+        ReadChannel ch = client.get(bucket, key).reader();
         return new InputStreamWithHints(
-                new BufferedInputStream(new FileInputStream(tempFile)),
+                new ResumableInputStream(Channels.newInputStream(ch), new InputStreamReopener(client, bucket, key)),
                 String.format("gcs://%s/%s", bucket, key)
         );
     }
@@ -61,53 +49,26 @@ public class SingleFileProvider
     {
     }
 
-    private Void getRemoteContentsWithRetry(final File tempFile, final Storage client, final String bucket, final String key, int maxConnectionRetry)
+    static class InputStreamReopener
+            implements ResumableInputStream.Reopener
     {
-        try {
-            return retryExecutor()
-                    .withRetryLimit(maxConnectionRetry)
-                    .withInitialRetryWait(500)
-                    .withMaxRetryWait(30 * 1000)
-                    .runInterruptible(new RetryExecutor.Retryable<Void>() {
-                        @Override
-                        public Void call() throws IOException
-                        {
-                            Storage.Objects.Get getObject = client.objects().get(bucket, key);
-                            try (BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(tempFile))) {
-                                IOUtils.copy(getObject.executeMediaAsInputStream(), outputStream);
-                            }
-                            return null;
-                        }
+        private final Storage client;
+        private final String bucket;
+        private final String key;
 
-                        @Override
-                        public boolean isRetryableException(Exception exception)
-                        {
-                            return true;  // TODO
-                        }
-
-                        @Override
-                        public void onRetry(Exception exception, int retryCount, int retryLimit, int retryWait)
-                                throws RetryExecutor.RetryGiveupException
-                        {
-                            String message = String.format("GCS GET request failed. Retrying %d/%d after %d seconds. Message: %s",
-                                    retryCount, retryLimit, retryWait / 1000, exception.getMessage());
-                            if (retryCount % 3 == 0) {
-                                log.warn(message, exception);
-                            }
-                            else {
-                                log.warn(message);
-                            }
-                        }
-
-                        @Override
-                        public void onGiveup(Exception firstException, Exception lastException)
-                                throws RetryExecutor.RetryGiveupException
-                        {
-                        }
-                    });
+        InputStreamReopener(Storage client, String bucket, String key)
+        {
+            this.client = client;
+            this.bucket = bucket;
+            this.key = key;
         }
-        catch (RetryExecutor.RetryGiveupException | InterruptedException ex) {
-            throw Throwables.propagate(ex.getCause());
+
+        @Override
+        public InputStream reopen(long offset, Exception closedCause) throws IOException
+        {
+            ReadChannel ch = client.get(bucket, key).reader();
+            ch.seek(offset);
+            return Channels.newInputStream(ch);
         }
     }
 }
